2b52d6ab1f86e1af97c1f132094e969f27c5d24c
ArchieFlynnAda <archie.flynn@ada.ac.uk>
2024-10-22 14:53:40 +0000

    Appending to a file

--- stats.js
+++ stats.js
@@ -4,9 +4,35 @@ const inputFile = "10000-common-passwords.csv";
 const outputFile = "statistics.csv";
 const delimiter = ",";
 
+const passLength = {
+  1 : 0,
+  2 : 0,
+  3 : 0,
+  4 : 0,
+  5 : 0,
+  6 : 0,
+  7 : 0,
+  8 : 0,
+  9 : 0,
+  10 : 0,
+  11 : 0,
+  12 : 0,
+  13 : 0,
+  14 : 0,
+  15 : 0,
+  16 : 0,
+  17 : 0,
+  18 : 0,
+  19 : 0,
+  20 : 0,
+
+
+}
+  
 function deleteExistingOutputFile() {
   if (fs.existsSync(outputFile)) {
     fs.unlinkSync(outputFile);
+    console.log("File Deleted")
   }
 }
 
@@ -15,11 +41,27 @@ function processData() {
   const lines = data.split(/\n/);
 
   for (let line of lines) {
-    elements = line.split(delimiter);
-    console.log(elements);
+   let element = line.split(delimiter);
+    //console.log(element);
+    let length = element[1].length
+    if (length in passLength){
+      passLength[length] += 1;
+    }else {
+      passLength[length] = 1;
+    }
   }
+  return passLength;
+}
+
+function savePassLength(freq){
+  for (const length in freq){
+   const content = (`Char ${length} : Count ${freq[length]} \n`)
+   fs.appendFileSync(outputFile, content, "utf-8" );
+  }
+
 }
 
 // Main execution
 deleteExistingOutputFile(); 
-processData();
+const results = processData();
+savePassLength(results);
